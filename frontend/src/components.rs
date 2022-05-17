12af2e0c00b6cc9da92eaf870e78d36e50e8db8c
Mark Ruddy <1markruddy@gmail.com>
2022-05-17 21:24:47 +0100

    add initial input parsing for erc20 addresses and transactions

--- frontend/src/components.rs
+++ frontend/src/components.rs
@@ -1,6 +1,7 @@
 use log::{info, warn};
+use std::error::Error;
 use wasm_bindgen::JsCast;
-use web3::types::Address;
+use web3::types::{Address, H256};
 use web_sys::{EventTarget, HtmlInputElement, InputEvent};
 use yew::prelude::*;
 
@@ -30,18 +31,15 @@ pub enum Msg {
 
 pub struct SearchBar;
 
-fn is_address(value: &str) -> bool {
-    /*match let result_addr: Address = value.parse() {
-        Ok(_) => true,
-        Err(_) => false,
-    }*/
-    if let Address(_) = value.parse() {
-        return true;
-    }
-    false
+fn get_address(value: &str) -> Result<Address, Box<dyn Error>> {
+    let addr: Address = value.parse()?;
+    Ok(addr)
 }
 
-fn is_tx_hash() -> bool {}
+fn get_tx_hash(value: &str) -> Result<H256, Box<dyn Error>> {
+    let tx_hash: H256 = value.parse()?;
+    Ok(tx_hash)
+}
 
 impl Component for SearchBar {
     type Message = Msg;
@@ -66,6 +64,15 @@ impl Component for SearchBar {
             // TODO: for every change to the text parse out the content for the API parameters we're looking for
             // once a "full parameter" is found, attempt to make API calls using it
             // e.g. tx_hash, address
+            match get_address(&full_value) {
+                Ok(_) => info!("Found valid address"),
+                Err(e) => info!("Not a valid address: {}", e),
+            }
+
+            match get_tx_hash(&full_value) {
+                Ok(_) => info!("Found valid tx_hash"),
+                Err(e) => info!("Not a valid tx_hash: {}", e),
+            }
 
             Msg::InputValue(full_value)
         });
